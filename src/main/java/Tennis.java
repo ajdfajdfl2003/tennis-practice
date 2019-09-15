edf6134ad2d99dcce3321363407ae6e0784f0838
AngusWu <anguswu@simbo.com.tw>
2019-09-15 15:34:02 +0800

    0_0

---
+++ src/main/java/Tennis.java
@@ -0,0 +1,15 @@
+public class Tennis {
+
+    private final String playerA;
+    private final String playerB;
+
+    public Tennis(String playerA, String playerB) {
+
+        this.playerA = playerA;
+        this.playerB = playerB;
+    }
+
+    public String score() {
+        return "Love All";
+    }
+}
---
+++ src/test/java/TennisTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TennisTest {
+    @Test
+    public void when_0_0_then_Love_All() {
+        Tennis tennis = new Tennis("Tom", "Joe");
+        String score = tennis.score();
+
+        assertEquals("Love All", score);
+    }
+}

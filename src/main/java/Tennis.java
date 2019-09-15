3750b1d8f55f1cc7769ad2110f2c5ece3645e3a7
AngusWu <anguswu@simbo.com.tw>
2019-09-15 15:52:47 +0800

    refactor: introduce field scoreLookup

--- src/main/java/Tennis.java
+++ src/main/java/Tennis.java
@@ -1,21 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Tennis {
+
+    private static Map<Integer, String> scoreLookup = new HashMap<>();
+
+    static {
+        scoreLookup.put(1, "Fifteen");
+        scoreLookup.put(2, "Thirty");
+    }
 
     private final String playerA;
     private final String playerB;
     private int playerAGameTimes = 0;
 
     public Tennis(String playerA, String playerB) {
-
         this.playerA = playerA;
         this.playerB = playerB;
     }
 
     public String score() {
-        if (this.playerAGameTimes == 1) {
-            return "Fifteen Love";
-        }
-        if (this.playerAGameTimes == 2) {
-            return "Thirty Love";
+        if (this.playerAGameTimes > 0) {
+            return scoreLookup.get(this.playerAGameTimes) + " Love";
         }
         return "Love All";
     }

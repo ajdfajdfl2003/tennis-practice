9f3fd821968ca272e200b157be6386b49fe0f2ce
AngusWu <anguswu@simbo.com.tw>
2019-09-15 15:46:04 +0800

    refactor test

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -1,23 +1,24 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TennisTest {
+    private Tennis tennis;
+
+    @Before
+    public void setUp() {
+        tennis = new Tennis("Tom", "Joe");
+    }
+
     @Test
     public void when_1_0_then_Fifteen_Love() {
-        Tennis tennis = new Tennis("Tom", "Joe");
-
         tennis.playerAHit();
-        String score = tennis.score();
-
-        assertEquals("Fifteen Love", score);
+        assertEquals("Fifteen Love", tennis.score());
     }
 
     @Test
     public void when_0_0_then_Love_All() {
-        Tennis tennis = new Tennis("Tom", "Joe");
-        String score = tennis.score();
-
-        assertEquals("Love All", score);
+        assertEquals("Love All", tennis.score());
     }
 }

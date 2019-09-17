6bfa26c7f22ec3f6edd5cbf57929a2fd4d86fde6
AngusWu <anguswu@simbo.com.tw>
2019-09-17 23:03:12 +0800

    1_0

--- src/main/Java/Tennis.java
+++ src/main/Java/Tennis.java
@@ -1,5 +1,14 @@
 public class Tennis {
+    private int playerAGameTimes;
+
     public String score() {
+        if (playerAGameTimes == 1) {
+            return "Fifteen Love";
+        }
         return "Love All";
+    }
+
+    public void playerAHit() {
+        this.playerAGameTimes++;
     }
 }
--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -5,6 +5,13 @@ import static org.junit.Assert.assertEquals;
 public class TennisTest {
 
     @Test
+    public void when_1_0_then_Fifteen_Love() {
+        Tennis tennis = new Tennis();
+        tennis.playerAHit();
+        assertEquals("Fifteen Love", tennis.score());
+    }
+
+    @Test
     public void when_0_0_then_Love_All() {
         Tennis tennis = new Tennis();
         assertEquals("Love All", tennis.score());

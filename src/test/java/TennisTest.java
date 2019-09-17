800b397e871f4c4354ca9c3b01c608bd1f1002c1
AngusWu <anguswu@simbo.com.tw>
2019-09-17 23:05:01 +0800

    refactor testing: extract field

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -1,19 +1,28 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TennisTest {
+    private Tennis tennis;
+
+    @Before
+    public void setUp() {
+        tennis = prepareTennis();
+    }
+
+    private Tennis prepareTennis() {
+        return new Tennis();
+    }
 
     @Test
     public void when_1_0_then_Fifteen_Love() {
-        Tennis tennis = new Tennis();
         tennis.playerAHit();
         assertEquals("Fifteen Love", tennis.score());
     }
 
     @Test
     public void when_0_0_then_Love_All() {
-        Tennis tennis = new Tennis();
         assertEquals("Love All", tennis.score());
     }
 }
